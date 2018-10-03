7589aaff5533022f5c9f8e7a9e0e6d09e77d8e8f
Jose María Gómez Martín <josemariagomezmartin99@gmail.com>
2018-10-03 10:33:50 +0200

    Ejercicio 11 tema 4

---
+++ Tema 4/Ejercicio11.java
@@ -0,0 +1,22 @@
+public class Ejercicio11 {
+	public static void main(String[] args){
+		System.out.println("Este programa te los segundos que quedan para llegar a media noche");
+		int hora;
+		System.out.print("Introduce la hora: ");
+		hora = Integer.parseInt(System.console().readLine());
+		int minuto;
+		System.out.print("Introduce los minutos: ");
+		minuto = Integer.parseInt(System.console().readLine());
+		int horaRestante;
+		int minRestante;
+		int segRestante;
+		if (((hora < 0) || (hora >= 24)) || ((minuto > 59) || (minuto < 0))){
+			System.out.println("Hora introducida no válida");
+		}else {
+			horaRestante = (24 - hora) * 3600;
+			minRestante = (60 - minuto) * 60;
+			segRestante = horaRestante + minRestante;
+			System.out.println("Quedan " + segRestante + "s para medianoche");
+		}
+	}
+}
