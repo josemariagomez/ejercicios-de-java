e22f9e1507b31ad60b39ffb44e080a2dc0681b19
Jose María Gómez Martín <josemariagomezmartin99@gmail.com>
2018-10-09 14:02:30 +0200

    Ejercicio 3 tema 4

--- Tema 5/Ejercicio02.java
+++ Tema 5/Ejercicio02.java
@@ -1,9 +1,9 @@
 public class Ejercicio02 {
 	public static void main(String[] args){
 		int i = 0;
-		while (i <= 20){
+		do{
 			System.out.println(i * 5);
 			i++;
-		}
+		}while (i <= 20);
 	}
 }
