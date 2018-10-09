5aa2aa424e818f4d26da3d952d67acd5d7f982df
Jose María Gómez Martín <josemariagomezmartin99@gmail.com>
2018-10-09 13:59:35 +0200

    Actualizando ejercicios 1 y 2 tema 4

--- Tema 5/Ejercicio02.java
+++ Tema 5/Ejercicio02.java
@@ -1,7 +1,9 @@
 public class Ejercicio02 {
 	public static void main(String[] args){
-		for (int i = 0 ; i <= 20 ; i++){
+		int i = 0;
+		while (i <= 20){
 			System.out.println(i * 5);
+			i++;
 		}
 	}
 }
