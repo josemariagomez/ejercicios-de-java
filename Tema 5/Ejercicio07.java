de91ebceb613beb03b1397394ba508b1b7df551e
Jose María Gómez Martín <josemariagomezmartin99@gmail.com>
2018-10-09 21:15:41 +0200

    Actualizando ejercicios tema 5

---
+++ Tema 5/Ejercicio07.java
@@ -0,0 +1,22 @@
+public class Ejercicio07 {
+	public static void main(String[] args){
+		int combinacion = 2818;
+		boolean respuesta = false;
+		int intentos = 1;
+		int numIntento;
+		System.out.println("Introduzca la combinación de la caja: ");
+		numIntento = Integer.parseInt(System.console().readLine());
+		do {
+			if (numIntento == combinacion){
+				System.out.println("La caja fuerte se ha abierto satisfactoriamente");
+				respuesta = true;
+			} else {
+				System.out.println("Lo siento, esa no es la combinación");
+				System.out.println("Inténtalo de nuevo: ");
+				numIntento = Integer.parseInt(System.console().readLine());
+				++intentos;
+			}
+		}while((respuesta == false) && (intentos < 4));
+		
+	}
+}
