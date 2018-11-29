ca376ac24926a48b73d0b188c648128c711d24be
Jose María Gómez Martín <josemariagomezmartin99@gmail.com>
2018-11-29 08:28:09 +0100

    Ejercicio18

--- Tema 8 - Funciones/Ejercicio18.java
+++ Tema 8 - Funciones/Ejercicio18.java
@@ -1,21 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tema.pkg8;
+
+import java.util.Scanner;
 
 /**
  *
  * @author jose_
  */
 public class Ejercicio18 {
-
-  /**
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner s = new Scanner(System.in);
+    
+    System.out.print("Introduce un número decimal: ");
+    int decimal = Integer.parseInt(s.nextLine());
+    int binario = 4;
+    
+    for (int i = decimal; i > 1; i /= 2) {
+      binario = (binario * 10) + (i % 2);
+      if (i < 4) {
+        binario = (binario * 10) + (i / 2);
+      }
+    }
+    binario = (Ejercicio01.voltea(binario) / 10);
+    System.out.println("En decimal corresponde al: " + binario);
   }
-  
 }
